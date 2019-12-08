59abca296553be8dda731d670dd44811d8d27c1e
Duncan Sparks <duncan.sprks@gmail.com>
2019-12-07 22:59:28 -0800

    hmm

--- vgdc_bot.ts
+++ vgdc_bot.ts
@@ -194,7 +194,7 @@ BotClient.on("message", (receivedMessage) => {
 
 
 BotClient.on("messageReactionAdd", (messageReaction, user) => {
-	if (messageReaction.message.channel.id === ChannelReactions) {
+	if (messageReaction.message.channel.id === "653120059262369793") {
 		switch (messageReaction.emoji.identifier) {
 			case ReactionID1:
 				messageReaction.message.guild.fetchMember(user).then((member) => member.addRole(Role1));
